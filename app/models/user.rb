0920ce9fac8727910ed8bbc4fb277aee8b0336f5
Tiago Tiede <geschopf@gmail.com>
2017-07-15 16:33:07 -0300

    Users CRUD

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+
+  #Validations
+  validates_presence_of :address
+
+  # Kaminari
+  paginates_per 5
+
+  # Associations
+  has_one :address
+
+  accepts_nested_attributes_for :address, update_only: true
+
+
+  def as_json(options={})
+    h = super(options)
+    h
+  end
+end
