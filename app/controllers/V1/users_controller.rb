c0de78a6baf5a7477b427717ad0bd8b395ec2dba
Tiago Tiede <geschopf@gmail.com>
2017-07-15 16:39:50 -0300

    Address modal

---
+++ app/controllers/V1/users_controller.rb
@@ -0,0 +1,53 @@
+module V1
+  class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :update, :destroy]
+
+    # GET /users
+    def index
+      @users = User.last(5)
+
+      render json: @users 
+    end
+
+    # GET /users/1
+    def show
+      render json: @user, include: [:address 
+    end
+
+    # POST /users
+    def create
+      @user = User.new(user_params)
+
+      if @user.save
+        render json: @user, include: [:kind, :phones, :address],  status: :created, location: @user
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    end
+
+    # PATCH/PUT /users/1
+    def update
+      if @user.update(user_params)
+        render json: @user, include: [:kind, :phones, :address]
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    end
+
+    # DELETE /users/1
+    def destroy
+      @user.destroy
+    end
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_user
+        @user = User.find(params[:id])
+      end
+
+      # Only allow a trusted parameter "white list" through.
+      def user_params
+        ActiveModelSerializers::Deserialization.jsonapi_parse(params)
+      end
+  end
+end
